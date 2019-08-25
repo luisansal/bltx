793e285a8b6fc00728b5e434e62760564236fc97
luisansal <luisansaldev@gmail.com>
2019-08-25 04:31:48 -0500

    cleaning structure

--- app/src/main/java/com/luisansal/bltx/common/exception/AuthorDuplicadoException.kt
+++
@@ -1,7 +0,0 @@
-package com.luisansal.bltx.common.exception
-
-import com.luisansal.bltx.model.domain.Author
-
-
-class AuthorDuplicadoException(val author : Author) : Exception("Author ${author.nombre} con DNI ${author.dni} duplicado ") {
-}
--- app/src/main/java/com/luisansal/bltx/model/domain/Order.kt
+++
@@ -1,7 +0,0 @@
-package com.luisansal.bltx.model.domain
-
-/**
- * Created by Luis on 14/10/2017.
- */
-
-class Order
